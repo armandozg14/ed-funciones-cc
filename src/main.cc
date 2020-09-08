bef32ad81659c8b6904f9b125513b6d7e86a4eac
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-09-08 11:05:35 -0700

    Funciones

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,18 @@
 #include<iostream>
-int main()
+
+void PrintHello()
 {
     std::cout <<"hello world";
+}
+
+int Add(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    PrintHello();
+    std::cout << "Add: " << Add(1,2) << std:: endl;
     std::cin.get();
     return 0;
 }
